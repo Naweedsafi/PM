083be78e15af39afe529c574250d111154e8e3c9
13018 <13018@10.0.0.99>
2021-01-30 23:05:49 -0500

    Demo updated

--- GitDemo/src/GitDemoClass.java
+++ GitDemo/src/GitDemoClass.java
@@ -1,9 +1,22 @@
+import com.learn.java.Animal;
 
 public class GitDemoClass {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
-
+		System.out.println("I am sleeping \" I am sleeping");{
+		}
+		Animal cat = new Animal();
+		cat.sleep();
+		cat.eat();
+		}
+		
+	
+		public void eat(){
+			System.out.println("I am eating cham cham..");
+			
+		}
+		public void sleep() {
+			System.out.println("I am sleeping...");
+		
+		}
 	}
-
-}
